0d3c122e2e85a4e7c2e9a8b10ade5a04e33085fb
Mitchell Parker <56657842+mitch-parker@users.noreply.github.com>
2023-03-13 22:53:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,91 @@
 import streamlit as st
 
-# function to calculate Wells' Criteria score
-def calculate_wells_criteria_score(clinical_features):
-    
-    wells_score = 0
-    
-    if clinical_features["clinical_symptoms"] == "Yes":
-        wells_score += 3
-        
-    if clinical_features["alternative_diagnosis"] == "No":
-        wells_score += 3
-        
-    if clinical_features["heart_rate"] >= 100:
-        wells_score += 1
-        
-    if clinical_features["immobilization"] == "Yes":
-        wells_score += 1
-        
-    if clinical_features["previous_dvt_pe"] == "Yes":
-        wells_score += 1
-        
-    if clinical_features["hemoptysis"] == "Yes":
-        wells_score -= 1
-        
-    if clinical_features["malignancy"] == "Yes":
-        wells_score += 1
-        
-    if wells_score < 0:
-        wells_score = 0
-        
-    return wells_score
+def calculate_wells_score(active_cancer, bedridden_surgery, calf_swelling, collateral_veins, entire_leg_swollen, localized_tenderness, pitting_edema, paralysis_paresis_immobilization, previously_documented_dvt, alternative_diagnosis):
+    """
+    Calculate the Wells' score based on the input data.
+    """
+    score = 0
+    if active_cancer == "Yes":
+        score += 1
+    if bedridden_surgery == "Yes":
+        score += 1
+    if calf_swelling == "Yes":
+        score += 1
+    if collateral_veins == "Yes":
+        score += 1
+    if entire_leg_swollen == "Yes":
+        score += 1
+    if localized_tenderness == "Yes":
+        score += 1
+    if pitting_edema == "Yes":
+        score += 1
+    if paralysis_paresis_immobilization == "Yes":
+        score += 1
+    if previously_documented_dvt == "Yes":
+        score += 1
+    if alternative_diagnosis == "Yes":
+        score -= 2
+    return score
 
-st.title("Wells' Criteria for DVT Calculator")
+def get_risk_group(score):
+    """
+    Determine the risk group based on the Wells' score.
+    """
+    if score <= 0:
+        return "Low/unlikely"
+    elif score <= 2:
+        return "Moderate"
+    else:
+        return "High/likely"
 
-clinical_symptoms = st.selectbox("Clinical symptoms of DVT?", ["Yes", "No"])
-alternative_diagnosis = st.selectbox("Is an alternative diagnosis more likely than DVT?", ["Yes", "No"])
-heart_rate = st.number_input("Heart rate (beats/minute)", value=60, min_value=0, max_value=300, step=1)
-immobilization = st.selectbox("Immobilization or surgery in the previous 4 weeks?", ["Yes", "No"])
-previous_dvt_pe = st.selectbox("Previous DVT or PE?", ["Yes", "No"])
-hemoptysis = st.selectbox("Hemoptysis?", ["Yes", "No"])
-malignancy = st.selectbox("Known active malignancy?", ["Yes", "No"])
+def get_prevalence(risk_group):
+    """
+    Determine the prevalence of DVT based on the risk group.
+    """
+    if risk_group == "Low/unlikely":
+        return "5%"
+    elif risk_group == "Moderate":
+        return "17%"
+    else:
+        return "17-53%"
 
-clinical_features = {
-    "clinical_symptoms": clinical_symptoms,
-    "alternative_diagnosis": alternative_diagnosis,
-    "heart_rate": heart_rate,
-    "immobilization": immobilization,
-    "previous_dvt_pe": previous_dvt_pe,
-    "hemoptysis": hemoptysis,
-    "malignancy": malignancy
+# Name conversion dictionary
+name_conversion = {
+    "Active cancer (treatment or palliation within 6 months)": "active_cancer",
+    "Bedridden recently >3 days or major surgery within 12 weeks": "bedridden_surgery",
+    "Calf swelling >3 cm compared to the other leg (measured 10 cm below tibial tuberosity)": "calf_swelling",
+    "Collateral (nonvaricose) superficial veins present": "collateral_veins",
+    "Entire leg swollen": "entire_leg_swollen",
+    "Localized tenderness along the deep venous system": "localized_tenderness",
+    "Pitting edema, confined to symptomatic leg": "pitting_edema",
+    "Paralysis, paresis, or recent plaster immobilization of the lower extremity": "paralysis_paresis_immobilization",
+    "Previously documented DVT": "previously_documented_dvt",
+    "Alternative diagnosis to DVT as likely or more likely": "alternative_diagnosis"
 }
 
-wells_score = calculate_wells_criteria_score(clinical_features)
+st.title("Wells' Criteria for DVT")
+st.markdown('---')
 
-st.write("Wells' Criteria score:", wells_score)
+# Create input form
+input_data = {}
+i = 0
+col1, col2 = st.columns(2)
+for question, key in name_conversion.items():
+    if i % 2:
+        col = col1
+    else:
+        col = col2
+    answer = col.radio(question, ("No", "Yes"), key=key)
+    input_data[key] = answer
+    i += 1
 
+# Calculate Wells' score and display results
+wells_score = calculate_wells_score(**input_data)
+risk_group = get_risk_group(wells_score)
+prevalence = get_prevalence(risk_group)
+
+# Display the results.
+st.markdown('---')
+st.success(f'**Wells\' Score:** {wells_score}')
+st.success(f'**Risk Group:** {risk_group}')
+st.success(f'**Prevalence of DVT:** {prevalence}')
